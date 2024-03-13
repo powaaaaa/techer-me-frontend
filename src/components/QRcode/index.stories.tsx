485318beb70c05c615857cbc97c1cc4ff4ca5ac9
Semi-koron <6jimoto6@gmail.com>
2024-03-14 00:49:18 +0900

    create QRcode component

---
+++ src/components/QRcode/index.stories.tsx
@@ -0,0 +1,19 @@
+import { QRcode } from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof QRcode> = {
+  component: QRcode,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof QRcode>;
+
+export const Default: Story = {
+  args: {},
+};
