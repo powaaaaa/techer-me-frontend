b823cd58588e6a7a2c04195db0bd73b021d53fe7
powaaaaa <knyuki2525@gmail.com>
2024-03-13 17:29:01 +0900

    add: test button

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,19 @@
+import { Button } from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export const Button:React.FC<Props> = ({}) => {
+  return <>this is ui of Button </>;
+};
