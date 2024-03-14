50a35ccf8233cdc5edd74bcd9180439678fc437d
powaaaaa <knyuki2525@gmail.com>
2024-03-14 17:54:21 +0900

    updat: Button

--- src/components/pages/TL/index.stories.tsx
+++ src/components/pages/TL/index.stories.tsx
@@ -1,13 +1,18 @@
-import { TL } from '.';
+import { TL } from ".";
 
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof TL> = {
   component: TL,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
+  render: (args) => (
+    <div className="w-[375px]">
+      <TL {...args} />
+    </div>
+  ),
 };
 
 export default meta;
@@ -15,5 +20,39 @@ export default meta;
 type Story = StoryObj<typeof TL>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    tlTitle: "Hack U 2024 Osaka",
+    posts: [
+      {
+        id: "1",
+        date: "3/12",
+        time: "14:55",
+        content: "Hellooooooooooooooooo, worrrrrrrrrrrrrrld!",
+      },
+      {
+        id: "wf",
+        date: "3/12",
+        time: "14:55",
+        content: "Hellooooooooooooooooo, worrrrrrrrrrrrrrld!",
+      },
+      {
+        id: "1tr",
+        date: "3/12",
+        time: "14:55",
+        content: "Hellooooooooooooooooo, worrrrrrrrrrrrrrld!",
+      },
+      {
+        id: "jyt",
+        date: "3/12",
+        time: "14:55",
+        content: "Hellooooooooooooooooo, worrrrrrrrrrrrrrld!",
+      },
+      {
+        id: "htrr",
+        date: "3/12",
+        time: "14:55",
+        content: "Hellooooooooooooooooo, worrrrrrrrrrrrrrld!",
+      },
+    ],
+  },
 };
--- src/components/pages/TL/index.tsx
+++ src/components/pages/TL/index.tsx
@@ -1,10 +1,30 @@
-type Props = {};
+import { PostType } from "@/components/Post";
+import { PostInput } from "@/components/PostInput";
+import { PostList } from "@/components/PostList";
+import { ArrowBack } from "@/components/icons/ArrowBack";
 
-export const TL: React.FC<Props> = ({}) => {
+type Props = {
+  tlTitle: string;
+  posts: PostType[];
+};
+
+export const TL: React.FC<Props> = ({ tlTitle, posts }) => {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className="flex">
+        <button>
+          <ArrowBack />
+        </button>
+        <p className="font-bold">{tlTitle}</p>
+      </header>
+
+      <main className="px-6">
+        <PostList posts={posts} handleReply={() => {}} />
+      </main>
+
+      <footer className="fixed bottom-0 w-full">
+        <PostInput onFileInputChange={() => {}} />
+      </footer>
     </>
   );
 };
