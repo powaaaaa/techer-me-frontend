7e4cbc98030e56e0b96637e09163d47e58a669d9
powaaaaa <knyuki2525@gmail.com>
2024-03-15 14:23:29 +0900

    update: storybook

--- src/components/pages/Join/hooks/index.ts
+++ src/components/pages/Join/hooks/index.ts
@@ -21,18 +21,19 @@ const defaultEvent: EventInfoType = {
 };
 
 //NOTE:  このdemoはbackendと繋げられたら消す
-const demo: EventInfoType = {
+const demoEvent: EventInfoType = {
   name: "HackU 2024 Osaka",
   startDate: "2024/03/15 10:00",
   endDate: "2024/03/16 18:00",
   message:
     "こんにちは！注意事項はhogehogehogehogehogefugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafuga",
 };
+const demoIcon = "https://avatars.githubusercontent.com/u/88587703?s=48&v=4";
 
 export const useJoinPage = (): UseJoinPage => {
-  const [userIcon, setUserIcon] = useState<string>("");
   //NOTE:  このdemoはbackendと繋げられたら消す
-  const [event, setEvent] = useState<EventInfoType>(demo);
+  const [userIcon, setUserIcon] = useState<string>(demoIcon);
+  const [event, setEvent] = useState<EventInfoType>(demoEvent);
 
   const handleJoinEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
--- src/components/pages/Join/index.stories.tsx
+++ src/components/pages/Join/index.stories.tsx
@@ -20,12 +20,5 @@ export default meta;
 type Story = StoryObj<typeof JoinPage>;
 
 export const Default: Story = {
-  args: {
-    userIcon: "https://avatars.githubusercontent.com/u/88587703?s=48&v=4",
-    eventName: "HackU 2024 Osaka",
-    startDate: "2024/03/15 10:00",
-    endDate: "2024/03/16 18:00",
-    eventMessage:
-      "こんにちは！注意事項はhogehogehogehogehogefugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafugafuga",
-  },
+  args: {},
 };
